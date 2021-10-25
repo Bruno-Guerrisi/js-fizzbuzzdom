007958af0fe56874b94c36b94a00efdfba29dd77
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-10-25 16:43:03 +0200

    append

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 let container = document.querySelector(".container-box");
 
+
 for( let i = 1; i <= 100; i++ ){
+
+    let boxNumber = document.createElement('div'); /* `<div class="box">${i}`; */
+    boxNumber.classList.add ("box");
+
+    if( (i % 3 == 0) && (i % 5 == 0) ) {
+
+        boxNumber.classList.add ("box-divs-3-5");
+        boxNumber.append('fizzbuzz');
+
+    } else if(i % 3 == 0) {
+
+        boxNumber.classList.add ("box-divs-3");
+        boxNumber.append('fizz');
+
+    } else if(i % 5 == 0) {
+
+        boxNumber.classList.add ("box-divs-5");
+        boxNumber.append('buzz');
+
+    } else {
+        boxNumber.append(`${i}`);
+    }
+
+    container.append(boxNumber);
+}
+
+
+/********* 
+  metodo 2 
+**********/
+/* for( let i = 1; i <= 100; i++ ){
 
     let boxNumber = `<div class="box">${i}`;
 
@@ -20,6 +52,6 @@ for( let i = 1; i <= 100; i++ ){
 
     boxNumber += '</div>';
 
-    container.innerHTML += boxNumber;
+    container.innerHTML += boxNumber; 
 
-}
+} */
